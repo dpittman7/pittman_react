51d083a616eafefc5fba2d646d0d5c102ec50969
dpittman7 <deantapittman@yahoo.com>
2023-02-08 16:46:40 -0600

    resume fix

--- src/components/Home.js
+++ src/components/Home.js
@@ -15,12 +15,9 @@ import {
 
 
 function Rig() {
-
     return useFrame((state) => {
-        if (true) {
             state.camera.position.x = THREE.MathUtils.lerp(state.camera.position.x, 1 + state.mouse.x / 4, 0.075)
             state.camera.position.y = THREE.MathUtils.lerp(state.camera.position.y, 1.5 + state.mouse.y / 4, 0.075)
-        }
 
 
     })
